efe09de4696b1967c879e4f5dcaf7d262a04ba71
Eliot6001 <51454033+Eliot6001@users.noreply.github.com>
2019-06-08 10:32:31 +0100

    Added a Loop
    Added a loop so the calculator doesn't exit after the result :) 
    Also Tried to make it look nicer.

--- er.py
+++ er.py
@@ -6,17 +6,17 @@ def mult(x, y): #*Function
  return      float(x) * float(y)
 def devi(x, y): #/function
  return      float(x) / float(y)
-x = input("Please Enter the first number: ") #1stnumber
-z = input("Please enter the operator: ") #operator
-y = input("Please Enter the Second number: ") #2ndnumber
 
-if z == "+":
-   print( sum(x, y))
-
-if z == "-":
-   print( neg(x, y))
-
-if z == "*":
-    print(mult(x, y))
-if z == "/":
-    print(devi(x, y))
+i = 1
+while i <= 999:
+    x = input("Please Enter the first number: ") #1stnumber
+    z = input("Please enter the operator: ") #operator
+    y = input("Please Enter the Second number: ") #2ndnumber
+    if z == "+":
+       print("The result is: " + str(sum(x, y)))
+    if z == "-":
+       print("The Result is: " + str(neg(x, y)))
+    if z == "*":
+        print("The Result is: " + str(mult(x, y)))
+    if z == "/":
+        print("The Result is: " + str(devi(x, y)))
